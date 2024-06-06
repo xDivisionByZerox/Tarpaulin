78a9eb6313fb40bbebdb5c76d51509377cdad4ec
Matthew <mathonich@yahoo.com>
2024-06-06 16:18:23 -0700

    Mongo -> Mongoose

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,33 +1,43 @@
-const { MongoClient } = require('mongodb')
+const mongoose = require('mongoose');
 
-const mongoHost = process.env.MONGO_HOST
-const mongoPort = process.env.MONGO_PORT
-const mongoUser = process.env.MONGO_USER
-const mongoPassword = process.env.MONGO_PASSWORD
-const mongoDbName = process.env.MONGO_DB_NAME
-const mongoAuthDbName = process.env.MONGO_AUTH_DB_NAME
+const mongoHost = process.env.MONGO_HOST;
+const mongoPort = process.env.MONGO_PORT;
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoDbName = process.env.MONGO_DB_NAME;
+const mongoAuthDbName = process.env.MONGO_AUTH_DB_NAME || mongoDbName
 
-const mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoAuthDbName}`
+// Connection URI
+const mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoAuthDbName}?authSource=${mongoDbName}`;
 
-let db = null
-let _closeDbConnection = null
+// Connection options
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Database connection
 exports.connectToDb = function (callback) {
-  MongoClient.connect(mongoUrl, function (err, client) {
-    if (err) {
-      throw err
-    }
-    db = client.db(mongoDbName)
-    _closeDbConnection = function () {
-      client.close()
-    }
-    callback()
-  })
-}
+  mongoose.connect(mongoUrl, options)
+    .then(() => {
+      console.log('Mongoose connection successful');
+      if (callback) callback(null, mongoose.connection);
+    })
+    .catch((err) => {
+      console.error('Mongoose connection error:', err);
+      if (callback) callback(err);
+    });
+};
 
+// Get database instance
 exports.getDbReference = function () {
-  return db
-}
+  return mongoose.connection;
+};
 
+// Close database connection
 exports.closeDbConnection = function (callback) {
-  _closeDbConnection(callback)
-}
+  mongoose.connection.close(() => {
+    console.log('Mongoose connection closed');
+    if (callback) callback();
+  });
+};
