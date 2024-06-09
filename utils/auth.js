34d7e983426192df4287ba2ca7fc557ac51f91f3
Matthew <mathonich@yahoo.com>
2024-06-08 23:18:45 -0700

    Idk if i need resolve/reject

--- utils/auth.js
+++ utils/auth.js
@@ -4,7 +4,7 @@ const { User } = require('../models/user')
 const secret_key = process.env.JWT_SECRET
 
 function generateToken(user_id) {
-    return new Promise((resolve, reject) => {
+    return new Promise(() => {
         return jwt.sign({ user_id }, secret_key, { expiresIn: '24h' });
     });
 }
