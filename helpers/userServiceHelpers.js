06ef714a28105053df95f989acc5704c60691f0c
Matthew <mathonich@yahoo.com>
2024-06-08 22:43:05 -0700

    ?

--- helpers/userServiceHelpers.js
+++ helpers/userServiceHelpers.js
@@ -4,7 +4,7 @@ const { extractValidFields } = require('../utils/validation.js');
 const bcrypt = require('bcrypt');
 
 
-module.exports.isAuthorizedToCreateUser = function(role, auth_role) {
+module.exports.isAuthorizedToCreateUser = async function(role, auth_role) {
   if (typeof(role) != 'string' || typeof(auth_role) != 'string') {
     throw new ValidationError('The request body was either not present or did not contain a valid User object.');
   }
