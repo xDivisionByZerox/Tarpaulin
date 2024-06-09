dea5b4dffd5a624a2ef12b5ab5d02409d452e6ef
Matthew <mathonich@yahoo.com>
2024-06-09 13:33:33 -0700

    Servererrors if not connected

--- utils/ratelimiter.js
+++ utils/ratelimiter.js
@@ -2,20 +2,24 @@ require('dotenv').config();
 const redisHost = process.env.REDIS_HOST || 'localhost';
 const redisPort = process.env.REDIS_PORT || 6379;
 const redis = require('redis');
-
-
+const { ServerError } = require('./error');
 
 let redisClient;
 
-module.exports.connectToRedis = async function connectToRedis() {
+module.exports.connectToRedis = async () => {
     try{
         redisClient = redis.createClient({
             url: `redis://${redisHost}:${redisPort}`
         });
         await redisClient.connect();
     } catch (err) {
-        console.error('Error connecting to Redis:', err);
-        process.exit(1);
+        throw new ServerError('Error connecting to Redis:', err);
     }
 }
 
+module.exports.getRedisClient = () => {
+    if (!redisClient) {
+        throw new ServerError('Redis client not initialized');
+    }
+    return redisClient;
+}
