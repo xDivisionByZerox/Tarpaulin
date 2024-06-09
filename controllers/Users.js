893470965d4136d48113c1822d8a27ad96d14df2
Matthew <mathonich@yahoo.com>
2024-06-09 14:26:36 -0700

    Then chaining for clarity

--- controllers/Users.js
+++ controllers/Users.js
@@ -4,39 +4,35 @@ var utils = require('../utils/writer.js');
 var Users = require('../service/UsersService');
 const { requireAuth, checkPermissions } = require('../utils/auth.js');
 const { errorHandler }= require('../middleware/errorHandler');
+const { rateLimiter } = require('../utils/ratelimiter.js');
 
 
 // Controllers call coresponding services, then passes response to Json writer to create response
 
 module.exports.authenticateUser = function authenticateUser (req, res, next, body) {
   // Logs in a user if the user exists and their credentials are correct
-  Users.authenticateUser(body)
-
-    .then(function (response) {
+  rateLimiter(req, res, next)
+    .then(() => Users.authenticateUser(body))
+    .then((response) => {
       utils.writeJson(res, response);
     })
+    .catch((error) => {
+      errorHandler(res, error);
+    })
+};
 
-    .catch(function (error) {
+module.exports.createUser = function createUser(req, res, next, body) {
+  rateLimiter(req, res, next)
+    .then(() => checkPermissions(req, res, next, body))
+    .then(() => Users.createUser(body))
+    .then((response) => {
+      utils.writeJson(res, response);
+    })
+    .catch((error) => {
       errorHandler(res, error);
     });
 };
 
-module.exports.createUser = function createUser (req, res, next, body) {
-  // If user has permissions to create a user, runs create user service
-  checkPermissions(req, res, next, body)
-
-    .then(() => {
-      return Users.createUser(body);
-    })
-
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-
-    .catch(function (error) {
-      errorHandler(res, error);
-    })
-};
 
 module.exports.getUserById = function getUserById (req, res, next, id) {
   // Gets a user by their id if their jwt id matches the id
