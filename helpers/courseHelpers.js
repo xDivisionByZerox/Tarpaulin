3eae856d5de4e26d125598ad107726b104deb4d0
Matthew <mathonich@yahoo.com>
2024-06-10 15:07:50 -0700

    Check for existing course

--- helpers/courseHelpers.js
+++ helpers/courseHelpers.js
@@ -1,8 +1,15 @@
 
-const { PermissionError } = require('../utils/error.js');
+const { PermissionError, ConflictError } = require('../utils/error.js');
 
 module.exports.isAuthorizedToCreateCourse = (auth_role) => {
   if (auth_role != 'admin') {
     throw new PermissionError('The request was not made by an authenticated User satisfying the authorization criteria.');
   }
+}
+
+module.exports.checkForExistingCourse = async (body) => {
+  const existingCourse = await Course.findOne(body);
+    if (existingCourse) {
+      throw new ConflictError('A course with the specified fields already exists.');
+    }
 }
